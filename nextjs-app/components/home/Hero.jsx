6d20c4cd980ed8de6cb7baf9497dc05b1ceacd43
GaziRiad <ouhibimustapha36@gmail.com>
2024-12-03 11:58:36 +0100

    adjusted spacing in hero section

--- nextjs-app/components/home/Hero.jsx
+++ nextjs-app/components/home/Hero.jsx
@@ -1,6 +1,6 @@
 function Hero({ title }) {
   return (
-    <section className="mx-auto flex max-w-[1568px] px-4 pt-56">
+    <section className="mx-auto flex max-w-[1568px] px-4 pt-44 2xl:pt-56">
       <h1 className="max-w-xl text-4xl leading-[116.667%] text-light-200 lg:text-[52px] 2xl:max-w-5xl 2xl:text-7xl">
         {title}
       </h1>
