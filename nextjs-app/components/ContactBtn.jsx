4d4dcfe74f549b3f84aa06fae8d30ce89e95bf55
GaziRiad <ouhibimustapha36@gmail.com>
2024-12-09 00:44:33 +0100

    animating green pulse on contact us button

--- nextjs-app/components/ContactBtn.jsx
+++ nextjs-app/components/ContactBtn.jsx
@@ -4,10 +4,11 @@ function ContactBtn({ label = "Contact us" }) {
   return (
     <Link
       href="/contact"
-      className="flex h-[34px] items-center gap-2 rounded-[4px] bg-white px-2 py-[10px] text-lg font-medium text-primary-700"
+      className="flex h-[34px] items-center gap-3 rounded-[4px] bg-white px-3 py-[10px] text-lg font-medium text-primary-700"
     >
       <span>{label}</span>
-      <span className="inline-block h-2 w-2 rounded-full bg-green-500" />
+      {/* <span className="inline-block h-2 w-2 rounded-full bg-green-500" /> */}
+      <span className="pulse"></span>
     </Link>
   );
 }
